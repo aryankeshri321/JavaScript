8194e5612f347c47e5e5f114f29eb4492a950366
Aryan Kumar Keshri <Aryankehsri00@gmail.com>
2025-11-12 11:16:31 +0530

    day 1

---
+++ Day_1.js
@@ -0,0 +1,80 @@
+// getName(); //Hello JavaScript
+// console.log(x); //undefined
+// var x = 10;
+// function getName() {
+//     console.log("hello JavaScript");
+// }
+
+// getName(); // Hello JavaScript
+// console.log(x); // ReferenceError: Cannot access 'x' before initialization
+// console.log(getName); [// ƒ: getName]
+// function getName() {
+//     console.log("hello JavaScript");
+// }
+
+// getName(); // TypeError: getName is not a function
+// console.log(getName);
+// var getName = function () {
+//     console.log("hello JavaScript");
+// };
+
+// var x = 1;
+// a(); // 10
+// b(); // 100
+// console.log(x); // 1
+
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
+
+// var x = 10;
+// console.log(x);
+// console.log(this.x);
+// console.log(window.x);
+
+// function nameCall() {
+//     console.log("aryan");
+// }
+// nameCall();
+
+// function nameCall_1() {
+//     let name = "kumar";
+//     console.log(name);
+// }
+// nameCall_1();
+
+// function rectangleArea(length, breadth) {
+//     let area = length * breadth;
+//     console.log(area);
+// }
+
+// function squareArea(side) {
+//     let area = side * side;
+//     console.log(area);
+// }
+// function circleArea(radius) {
+//     let area = 3.14 * radius * radius;
+//     console.log(area);
+// }
+
+// rectangleArea(20, 10);
+// squareArea(10);
+// circleArea(5);
+
+// fine si and ci with function and loop/
+function simpleInterest(p, r, t) {
+    let si = (p * r * t) / 100;
+    console.log("Simple Interest is: " + si);
+}
+function compoundInterest(p, r, t) {
+    let ci = p * (Math.pow(1 + r / 100, t)) - p;
+    console.log("Compound Interest is: " + ci);
+}
+simpleInterest(1000, 5, 2);
+compoundInterest(1000, 5, 2);
