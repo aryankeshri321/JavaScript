992e50aa45c28760662b2c917fc046fd7328c83d
Aryan Kumar Keshri <Aryankehsri00@gmail.com>
2025-11-21 13:40:29 +0530

    DAY_X

---
+++ Day_4.js
@@ -0,0 +1,48 @@
+// Print numbers from 10 to 1 for loop
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// Print numbers from 10 to 1 using while loop
+
+// let i = 10;
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
+
+//print even numbers from 1 to 20 using for loop
+// for (let i = 1; i <= 20; i++) {
+//     if ((i % 2) === 0 ) {
+//         console.log(i);
+//     }
+// }
+
+//print odd numbers from 1 to 15 using while loop
+
+// let i = 1;
+// while (i <= 15) {
+//     if (i % 2 === 1) {
+//         console.log(i);
+
+//     }
+//     i++;
+// }
+
+// Print multiplication table of 5 using for loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// fine the sum of no. from 1 to 100 using a loop
+
+// let sum = 0;
+
+// for (let i = 1; i <= 132541; i++) {
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
---
+++ day_3.js
@@ -0,0 +1,63 @@
+// repeat krna ko loop kahte hai
+// for while do-while foreach forin foroff
+
+// case 1
+// kaha se jaana hai --> kaha tak jaana hai --> kaise jaana hai
+// (for)loop
+// Ex:- 1 to 50 g
+// kaha se jaana hai --> kab rukna hai --> kaise jaana hai
+// while
+// tab tak chatna h jab tak --> (hello na aa jaye , laal color ka ped dekhna h)
+
+// for loop
+
+// for (start; end; change) {
+    // rest code
+// };
+
+// for (let i = 0; i <= 50; i++) {
+//     console.log(i);
+// }
+
+// while loop
+
+// start;
+// while(end){
+    // rest code
+    // change
+// }
+
+// let i = 1;
+// while(i <= 32){
+//     console.log(i);
+//     i++;
+// }
+
+// do-while loop
+
+// start;
+// do {
+//     // rest code
+//     // change
+
+// }while(end)
+
+// let i = 2;
+// do {
+//     console.log(i);
+//     i++;
+// }while(i <= 1);
+
+// for (let i = 1; i < 200; i++) {
+//     console.log(i);
+//     if (i === 100) {
+//         break;
+//     }
+// }
+
+// for (let i = 1; i < 200; i++) {
+//     if (i === 100) {
+//         continue;
+//     }
+//     console.log(i);
+// }
