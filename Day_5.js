b6bb49a3710f1c461d63661f19cf7222ccf5c972
Aryan Kumar Keshri <Aryankehsri00@gmail.com>
2025-11-21 23:23:38 +0530

    DAY_5

---
+++ Day_5.js
@@ -0,0 +1,38 @@
+// what is function
+// what Function
+
+// agir code likh diya ti wo turant chal jaata hai
+
+// console.log("hey");
+
+// function Kaise Banaye tarika
+    
+// function myfun() {
+    // function Statements
+// }
+
+// let fun = function () {
+   // function expetiction
+// }
+
+
+// let fun  = () => {
+// fat arrow fnc
+// }
+
+// Parameter and argument
+
+// function mufun(v1) {
+//     console.log(`${v1} naach raha hai`);
+// }
+
+// mufun("aryan");
+// mufun("Soni");
+// mufun("deepak");
+// mufun("pappu");
+
+function myfun(v1, v2, v3) {
+    console.log(v1 + v2 + v3);
+}
+
+myfun(34523,456234543,463563)
